49a66f4a590376330aa556a4df2339ec6eaf4c62
53101 <53101@edu.raseko.fi>
2026-08-18 13:55:32 +0300

    App.js and file management

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render("index.handlebars")
+});
+
+app.listen(PORT)
